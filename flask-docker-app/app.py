032bf9826832b25de1c06bf36239423d4c208ea9
Matt Stropkey <mas682@pitt.edu>
2023-12-22 16:27:05 -0500

    Added health check endpoint to app

--- flask-docker-app/app.py
+++ flask-docker-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
 
 app = Flask(__name__)
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return f'Hello, Dockerized Flask App! {os.environ.get("TEST_SECRET", "NOT FOUND")} {os.environ.get("LITERAL_SECRET", "NOT FOUND")}'
+
+@app.route('/health')
+def health_check():
+    # Perform any health check logic here
+    # For simplicity, just return a JSON response indicating the application is healthy
+    return jsonify(status='ok', message='Health check passed')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
