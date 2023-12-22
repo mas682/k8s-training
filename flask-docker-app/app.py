4ff570cc712d07aa5f05b1a1c9110f898fb32b1a
Matt Stropkey <mas682@pitt.edu>
2023-12-22 15:52:03 -0500

    Updates with git pull

--- flask-docker-app/app.py
+++ flask-docker-app/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Dockerized Flask App!'
+    return f'Hello, Dockerized Flask App! {os.environ.get("TEST_SECRET", "NOT FOUND")} {os.environ.get("LITERAL_SECRET", "NOT FOUND")}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
