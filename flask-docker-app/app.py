157a0c6490970a92de87b9d4ab5576a210ed2f8a
Matt Stropkey <mas682@pitt.edu>
2023-12-22 16:59:01 -0500

    Added readiness probe

--- flask-docker-app/app.py
+++ flask-docker-app/app.py
@@ -6,15 +6,16 @@ import time
 app = Flask(__name__)
 
 # Flag to indicate whether the health check should fail
-health_check_failed = False
+health_check_failed = True
 
-def simulate_health_check_failure():
+def simulate_health_check():
     global health_check_failed
-    time.sleep(60)  # Simulate a delay of 1 minute
-    health_check_failed = True
+    while True:
+        time.sleep(60)  # Simulate a delay of 1 minute
+        health_check_failed = not health_check_failed
 
 # Start the health check failure simulation in a separate thread
-thread = threading.Thread(target=simulate_health_check_failure)
+thread = threading.Thread(target=simulate_health_check)
 thread.start()
 
 @app.route('/')
