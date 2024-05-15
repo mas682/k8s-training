4e8beb041f69ee9bb6ee66e1743e5cd76d0339c4
Matt Stropkey <mas682@pitt.edu>
2024-05-15 11:30:16 -0400

    Added frontend service

--- app/frontend/main.py
+++ app/frontend/main.py
@@ -77,4 +77,4 @@ def health_check():
     return jsonify(status='ok', message='Health check passed')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
